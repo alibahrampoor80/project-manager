39a24548c02e67f6de056584ab5c8b0057003686
ali bahrampoor <ali.bahrampoor1380@gmail.com>
2023-06-05 20:00:46 +0330

    create project section

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -1,9 +1,15 @@
+const {projectModel} = require("../../models/project");
+
 class ProjectController {
     async createProject(req, res, next) {
         try {
             const {title, text} = req.body
+            const owner = req.user._id
+            const result = await projectModel.create({title, text, owner})
+            if (!result) throw {status: 400, message: "افزودن پروژه با مشکل مواجه شد"}
+            return res.status(201).json({status: 201, message: "پروژه با موفیقت ایجاد شد"})
         } catch (err) {
-
+            next(err)
         }
     }
 
--- app/routes/project.js
+++ app/routes/project.js
@@ -1,6 +1,9 @@
 const {ProjectController} = require("../http/controllers/project.controller");
+const {createProjectValidator} = require("../http/validation/project");
+const {ExpressValidatorMapper} = require("../http/middleware/checkErrors");
+const {checkLogin} = require("../http/middleware/autoLogin");
 const router = require('express').Router()
 
-router.post('/create',ProjectController.createProject)
+router.post('/create',checkLogin, createProjectValidator(), ExpressValidatorMapper, ProjectController.createProject)
 
 module.exports = {projectRoutes: router}
