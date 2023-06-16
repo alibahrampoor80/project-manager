290ffa5b48dc678c770eb940bb73edb301dfbce4
ali bahrampoor <ali.bahrampoor1380@gmail.com>
2023-06-16 17:19:56 +0330

    project create

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -4,8 +4,9 @@ class ProjectController {
     async createProject(req, res, next) {
         try {
             const {title, text, image, tags} = req.body
+            console.log(tags)
             const owner = req.user._id
-            const result = await projectModel.create({title, text, owner, image})
+            const result = await projectModel.create({title, text, owner, image, tags})
             if (!result) throw {status: 400, message: "افزودن پروژه با مشکل مواجه شد"}
             return res.status(201).json({status: 201, message: "پروژه با موفیقت ایجاد شد"})
         } catch (err) {
--- app/http/validation/project.js
+++ app/http/validation/project.js
@@ -3,7 +3,9 @@ const {body} = require("express-validator");
 function createProjectValidator() {
     return [
         body('title').notEmpty().withMessage('عنوان پروژه نمیتواند خالی باشد'),
-        body('text').notEmpty().isLength({min: 20}).withMessage('توضیحات پروژه نمیتواند خالی باشد حداقل باید 20 کارکتر باشد'),
+        body('text').notEmpty().isLength({min: 20})
+            .withMessage('توضیحات پروژه نمیتواند خالی باشد حداقل باید 20 کارکتر باشد'),
+        body('tags').isArray({min: 0, max: 10}).withMessage("تگ کترین 0 و بیشترین 10 تا میتواد باشد")
     ]
 }
 
